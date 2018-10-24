d9088afdbda93c692e6f061137591b5a56595827
edwin <edwinkun@gmail.com>
2018-10-24 16:35:39 +0700

    removing print on main page

--- main.go
+++ main.go
@@ -3,19 +3,15 @@ package main
 import (
 	"config"
 	"app"
-	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"  
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 )
 
 func main() {
 	tracer.Start(tracer.WithDebugMode(true))
-
-        fmt.Println("1111 1")
-
         myConfig := config.GetConfig()
 
         myApp := &app.App{}
         myApp.Initialize(myConfig)
         myApp.Run(":3000")
-        fmt.Println("22222 2")
         defer tracer.Stop()
 }
